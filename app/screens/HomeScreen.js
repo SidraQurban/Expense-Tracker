eeef78216748b8e4c11150f6612cf4a8b6c6ea3f
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-02-08 01:03:32 -0800

    updated

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,14 +1,43 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { responsiveFontSize, responsiveHeight } from 'react-native-responsive-dimensions';
 
 const HomeScreen = () => {
+const [data,setData] = useState(undefined);
+
+const getAPIData =async () =>{
+   const url = ("https://jsonplaceholder.typicode.com/posts/1");
+   let result = await fetch(url);
+   result = await result.json();
+   setData(result);
+}
+
+useEffect (() => {
+  getAPIData();
+},[])
+
   return (
     <View>
-      <SafeAreaView>
-        <Text>HomeScreen</Text>
-        
-      </SafeAreaView>
+      {data ? (
+        <View
+          style={{
+            marginTop: responsiveHeight(3),
+            padding: responsiveHeight(2),
+          }}
+        >
+          <Text>
+            {data.title}
+          </Text>
+          <Text style={{ fontSize: responsiveFontSize(2), fontWeight: "500" }}>
+            {data.body}
+          </Text>
+        </View>
+      ) : (
+        <View>
+          <Text>Data not found</Text>
+        </View>
+      )}
     </View>
   );
 }
