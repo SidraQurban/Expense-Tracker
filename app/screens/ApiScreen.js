86090cf10492316e61c66ac11ead3882641c9f58
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-02-10 18:42:04 -0800

    ./

--- app/screens/ApiScreen.js
+++ app/screens/ApiScreen.js
@@ -1,13 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { responsiveFontSize } from 'react-native-responsive-dimensions'
+import { responsiveFontSize, responsiveHeight } from 'react-native-responsive-dimensions'
 
 const ApiScreen = () => {
   return (
    <SafeAreaView>
-     <View>
-      <Text style={{fontSize:responsiveFontSize(2)}}>Getting API's</Text>
+     <View style={{marginTop:responsiveHeight(2)}}>
+      <Text style={{fontSize:responsiveFontSize(2), textAlign:"center"}}>Getting API's</Text>
     </View>
    </SafeAreaView>
   )
