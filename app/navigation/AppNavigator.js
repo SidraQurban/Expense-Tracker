79cd482bdd22e5c94b93f68b21427a911fd6cf98
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-02-10 18:41:19 -0800

    ./

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -2,14 +2,16 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { NavigationContainer } from '@react-navigation/native';
-import HomeScreen from '../screens/HomeScreen';
+// import HomeScreen from '../screens/HomeScreen';
+import ApiScreen from '../screens/ApiScreen';
 
 const Stack = createNativeStackNavigator();
 const AppNavigator = () => {
   return (
    <NavigationContainer>
     <Stack.Navigator screenOptions={{headerShown: false}}> 
-        <Stack.Screen name="Home" component={HomeScreen} />
+        {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
+        <Stack.Screen name="Api" component={ApiScreen} />
         
     </Stack.Navigator>
    </NavigationContainer>
