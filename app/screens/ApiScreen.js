3e749680094cf52b2352dd4dc2a110a34e23a38e
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-02-10 22:25:52 -0800

    ./

--- app/screens/ApiScreen.js
+++ app/screens/ApiScreen.js
@@ -1,18 +1,41 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { responsiveFontSize, responsiveHeight } from 'react-native-responsive-dimensions'
 
 const ApiScreen = () => {
 
-
   return (
    <SafeAreaView>
      <View style={{marginTop:responsiveHeight(2)}}>
-      <Text style={{fontSize:responsiveFontSize(2), textAlign:"center"}}>Getting API's</Text>
+      <Text style={{fontSize:responsiveFontSize(2), textAlign:"center"}}>Post API Call</Text>
+     
     </View>
    </SafeAreaView>
   )
 }
 
 export default ApiScreen
+
+
+
+
+
+// const saveApiData = async() => {
+//     const data = {
+//         name: "Ali",
+//         age:29, 
+//         email: "ali@test.com"
+//     }
+//   const url = "http://10.0.2.2:3000/users";
+//   let result = await fetch(url, {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body:JSON.stringify(data)
+//   });
+//   result = await result.json();
+//   console.warn(result);
+  
+// }
+
+{/* <Button   title="Save Data" onPress={saveApiData} /> */}
