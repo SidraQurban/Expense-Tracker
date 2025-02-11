a57439c818f0c44d8a3909805a79501f016ebd35
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-02-11 00:08:30 -0800

    ../

--- app/screens/ApiScreen.js
+++ app/screens/ApiScreen.js
@@ -1,7 +1,7 @@
 import { View, Text, Button, TextInput, TouchableOpacity } from 'react-native';
 import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { responsiveFontSize, responsiveHeight, responsiveWidth } from 'react-native-responsive-dimensions'
+import { responsiveFontSize, responsiveHeight, responsiveWidth } from 'react-native-responsive-dimensions';
 
 const ApiScreen = () => {
 const [name,setName] = useState("");
