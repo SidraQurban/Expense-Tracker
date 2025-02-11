38198e195f615e8b1e78d3a334cdcf32e64bf4f0
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-02-10 18:52:27 -0800

    ./

--- app/screens/ApiScreen.js
+++ app/screens/ApiScreen.js
@@ -1,15 +1,16 @@
-import { View, Text } from 'react-native'
-import React, { useEffect } from 'react'
+import { View, Text, FlatList } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { responsiveFontSize, responsiveHeight } from 'react-native-responsive-dimensions'
+import { responsiveFontSize, responsiveHeight, responsiveWidth } from 'react-native-responsive-dimensions'
 
 const ApiScreen = () => {
 
+const [data,setData] = useState([]);
 const getAPI =async () => {
     const url = "http://10.0.2.2:3000/users";
     let result = await fetch(url);
     result = await result.json();
-    console.warn(result);
+    setData(result);
     
 
 }
@@ -19,12 +20,26 @@ getAPI();
 },[])
 
   return (
-   <SafeAreaView>
-     <View style={{marginTop:responsiveHeight(2)}}>
-      <Text style={{fontSize:responsiveFontSize(2), textAlign:"center"}}>Getting API's</Text>
-    </View>
-   </SafeAreaView>
-  )
+    <SafeAreaView>
+      <View style={{ marginTop: responsiveHeight(2) }}>
+        <Text style={{ fontSize: responsiveFontSize(2), textAlign: "center" }}>
+          Getting API's
+        </Text>
+      </View>
+      {data.length ? (
+        <FlatList
+          data={data}
+          renderItem={({ item }) => (
+            <View style={{ flexDirection: "row", padding: responsiveWidth(2) }}>
+              <Text>{item.name}</Text>
+              <Text>{item.age}</Text>
+              <Text>{item.email}</Text>
+            </View>
+          )}
+        />
+      ) : null}
+    </SafeAreaView>
+  );
 }
 
 export default ApiScreen
