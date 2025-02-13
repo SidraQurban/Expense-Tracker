ae53ebcfd4500b3469e70b85e6ca3928a22c2c51
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-02-12 18:37:55 -0800

    ./

--- app/screens/SearchScreen.js
+++ app/screens/SearchScreen.js
@@ -1,12 +1,29 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput } from 'react-native'
+import React, { useState } from 'react'
+import { responsiveHeight, responsiveWidth } from 'react-native-responsive-dimensions'
 
 const SearchScreen = () => {
+const [data,setData] = useState([]);
+
+const searchUser = async () => {
+  const url = "http://10.0.2.2:3000/users";
+  let result = await fetch(url);
+  result = await result.json();
+  if (result) {
+    setData(result);
+  }
+};
   return (
-    <View>
-      <Text>SearchScreen</Text>
+    <View
+      style={{ marginTop: responsiveHeight(4), padding: responsiveWidth(5) }}
+    >
+      <TextInput
+        placeholder="Search Here..."
+        style={{ borderColor: "blue", borderWidth: 1 }}
+        onChangeText={(text) =>searchUser( text)}
+      />
     </View>
-  )
+  );
 }
 
 export default SearchScreen
