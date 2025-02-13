8808824614722c1166efbfaa34ee95f4d2a258d7
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-02-12 22:49:24 -0800

    /.

--- app/screens/SearchScreen.js
+++ app/screens/SearchScreen.js
@@ -5,23 +5,35 @@ import { responsiveHeight, responsiveWidth } from 'react-native-responsive-dimen
 const SearchScreen = () => {
 const [data,setData] = useState([]);
 
-const searchUser = async () => {
-  const url = "http://10.0.2.2:3000/users";
+const searchUser = async (text) => {
+  const url = `http://10.0.2.2:3000/users?q=${text}`;
+  console.warn(url);
+  
   let result = await fetch(url);
+  
   result = await result.json();
   if (result) {
     setData(result);
   }
 };
   return (
-    <View
-      style={{ marginTop: responsiveHeight(4), padding: responsiveWidth(5) }}
-    >
-      <TextInput
-        placeholder="Search Here..."
-        style={{ borderColor: "blue", borderWidth: 1 }}
-        onChangeText={(text) =>searchUser( text)}
-      />
+    <View>
+      <View
+        style={{ marginTop: responsiveHeight(4), padding: responsiveWidth(5) }}
+      >
+        <TextInput
+          placeholder="Search Here..."
+          style={{ borderColor: "blue", borderWidth: 1 }}
+          onChangeText={(text) => searchUser(text)}
+        />
+        {data.length
+          ? data.map((item) => 
+              <View key={item.id}>
+                <Text>{item.name}</Text>
+              </View>
+           )
+          : null}
+      </View>
     </View>
   );
 }
