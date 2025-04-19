a3a0b1b003daa0aaf2ecd16bf3b7961053934e18
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-04-18 23:53:57 -0700

    ./

--- app/screens/SearchScreen.js
+++ app/screens/SearchScreen.js
@@ -5,6 +5,7 @@ import { responsiveHeight, responsiveWidth } from 'react-native-responsive-dimen
 const SearchScreen = () => {
 const [data,setData] = useState([]);
 
+
 const searchUser = async (text) => {
   const url = `http://10.0.2.2:3000/users?q=${text}`;
   console.warn(url);  
