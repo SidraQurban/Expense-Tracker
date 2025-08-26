c49651f7b89286fd1e25cba55350faba0f5afa83
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-08-26 11:16:57 -0700

    ./

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,23 +1,22 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { NavigationContainer } from '@react-navigation/native';
+import { View, Text } from "react-native";
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
 // import HomeScreen from '../screens/HomeScreen';
 // import ApiScreen from '../screens/ApiScreen';
-import SearchScreen from '../screens/SearchScreen';
+import SearchScreen from "../screens/SearchScreen";
 
 const Stack = createNativeStackNavigator();
 const AppNavigator = () => {
   return (
-   <NavigationContainer>
-    <Stack.Navigator screenOptions={{headerShown: false}}> 
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
         {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
         {/* <Stack.Screen name="Api" component={ApiScreen} /> */}
-        <Stack.Screen name = "Search" component={SearchScreen} /> 
-        
-    </Stack.Navigator>
-   </NavigationContainer>
-  )
-}
+        <Stack.Screen name="Search" component={SearchScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
-export default AppNavigator
+export default AppNavigator;
